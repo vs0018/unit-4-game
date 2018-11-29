ea1caf93bec87a15ad0a559bb4e080ac7950fc94
Valerie S <43964677+vs0018@users.noreply.github.com>
2018-11-29 12:51:10 -0500

    pseudocode javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+//game is reset so score value is set to 0 and 'announce' text hidden
+
+//random number (19-120) is generated and displayed on page
+
+//random crystal values are generated (1-12)
+
+//player clicks on crystal, its value is added to display
+
+//total is compared to random number
+
+//if it is less than or matches, win is displayed, wins are incremented, and new numbers generated (game reset)
+
+//if it is greater than the random number, loss is displayed, losses are incremented, and new numbers generated (game reset)
