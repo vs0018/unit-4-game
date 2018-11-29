f144ef91535ed0464afffc38674e130f91be7f79
Valerie S <43964677+vs0018@users.noreply.github.com>
2018-11-29 14:04:56 -0500

    add basic logic for crystal clicks and if/else loop to compare to random number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,37 @@
 //VARIABLES
-var randomNumber = 0
-var playerTotal = 0
-var wins = 0
-var losses = 0
-var crystals = [c1, c2, c3, c4]
+var randomNumber = 0;
+var playerTotal = 0;
+var wins = 0;
+var losses = 0;
+// var crystals = [c1, c2, c3, c4]
 
 //game is reset so score value is set to 0 and 'announce' text hidden
 
 //random number (19-120) is generated and displayed on page
+$("#guessNumber").text(randomNumber);
 
 //random crystal values are generated (1-12)
 
 //player clicks on crystal, its value is added to display
+$(".crystal").on("click", function() {
+
+    alert("You clicked a crystal!");
 
 //total is compared to random number
+    if (playerTotal === randomNumber) {
 
-//if it is less than or matches, win is displayed, wins are incremented, and new numbers generated (game reset)
+//if it matches, win is displayed, wins are incremented, and new numbers generated (game reset)
+        $("#announce").text("You win!!");
+      } else if (playerTotal > randomNumber) {
+        $("#announce").text("You lose!!");
+      }
+
+  });
+
+$("#playScore").text(playerTotal);
+
+
+
+
 
 //if it is greater than the random number, loss is displayed, losses are incremented, and new numbers generated (game reset)
