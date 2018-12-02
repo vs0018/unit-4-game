3bcab8cc677cc20434774093edbc454840b5335b
Valerie S <43964677+vs0018@users.noreply.github.com>
2018-12-01 20:08:05 -0500

    finalize logic with some bugs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,13 +5,10 @@ $(document).ready(function() {
     var playerTotal;
 
 //function to generate a random number
-function getRandomNumber (minNumber, maxNumber) {
-    return Math.floor(Math.random() * (maxNumber + 1) + minNumber);
-}
+    function getRandomNumber (minNumber, maxNumber) {
+        return Math.floor(Math.random() * (maxNumber + 1) + minNumber);
+    };
 
-function reset(){
-    playerTotal = 0;
-    $("playScore").text("0");
 
 //random number (19-120) is generated and displayed on page
     var random = getRandomNumber(19, 120);
@@ -19,42 +16,48 @@ function reset(){
 //number is displayed on page
     $("#guessNumber").text(random);
 
-//Four random crystal values are generated (1-12)
+//Four random crystal values are generated (1-12) and assigned to each DOM element
     $(".crystal").each(function() {
         var crystalValue = getRandomNumber(1, 12);
         $(this).data("value", crystalValue);
     });
-};
 
-    //total is compared to random number
-function checkScore() {
+
+//player clicks on crystal to start interaction
+    $(".crystal").click(function() {
+
+//Function to take player total and compare it to the randomly generated number
+    function checkScore() {
         if (playerTotal === random) {
     
-    //if it matches, win is displayed, wins are incremented, and new numbers generated (game reset)
+//if it matches, win is displayed, wins are incremented, and new numbers generated (game reset)
             $("#announce").text("You win!!");
             wins++;
             $("#wins").text(wins);
-            reset();
           }
 
-    //if it is greater than the random number, loss is displayed, losses are incremented, and new numbers generated (game reset)      
+//if it is greater than the random number, loss is displayed, losses are incremented, and new numbers generated (game reset)      
           else if (playerTotal > random) {
             $("#announce").text("You lose!!");
             losses++;
             $("#losses").text(losses);
-            reset();
           }
     };
 
-//player clicks on crystal, its value is added to display
-$(".crystal").click(function() {
-    console.log($(this).data("value"));
-    var amount = $(this).data("value");
-    var currentScore = parseInt($("#playScore").text(), 10);
-    playerTotal = currentScore + amount;
-    $("#playScore").text(playerTotal);
-    checkScore();
-});
+//value of the clicked on crystal is console logged and stored in a variable
+        console.log($(this).data("value"));
 
-reset();
+        var amount = $(this).data("value");
+
+//the current score is grabbed from the page and converted to an integer
+        var currentScore = parseInt($("#playScore").text(), 10);
+
+//these amounts are added to get the new player total and it is displayed on the page
+        playerTotal = currentScore + amount;
+        $("#playScore").text(playerTotal);
+
+//function is called to check the score
+        checkScore();
+    });
+
 });
